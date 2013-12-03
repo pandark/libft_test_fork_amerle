380721fd5dadcf4bf1dcefc4856a838643f9ad22
Axel Viala <aviala@student.42.fr>
2013-12-03 23:33:32 +0100

    Adding test for ft_strchr and ft_strrchr for all of the print table.

--- srcs/ft_test_strchr.c
+++ srcs/ft_test_strchr.c
@@ -9,23 +9,27 @@ static int	ft_test_strchr2(char const *s, int c)
 	res = 0;
 	res2 = ft_strchr(s, c);
 	res3 = strchr(s, c);
-	if (res2 != res3)
+	if (res2 != res3) {
 		++res;
-	printf("Test : \"%s\" { %p - %p }", s, res2, res3);
+	}
+	printf("Test : \'%c\' in \"%s\" { %p - %p}", c, s, res2,res3);
 	ft_print_status(res);
 	return (res);
 }
 
 int	ft_test_strchr(void)
 {
-	int		res;
-	const char	*str = "Hello world !";
+	int			res;
+	const char		str[] = "Hello world !";
+	char		c;
 
 	res = 0;
 	ft_print_begin("ft_strchr");
+	c = 'a';
+	res += ft_test_strchr2(str, 'H');
+	while (c <= 'z')
+		res += ft_test_strchr2(str, c++);
 	res += ft_test_strchr2(str, '!');
 	res += ft_test_strchr2(str, 0);
-	res += ft_test_strchr2(str, 'H');
-	res += ft_test_strchr2(str, 'z');
 	return (ft_print_end(res));
 }
--- srcs/ft_test_strrchr.c
+++ srcs/ft_test_strrchr.c
@@ -9,23 +9,27 @@ static int	ft_test_strrchr2(char const *s, int c)
 	res = 0;
 	res2 = ft_strrchr(s, c);
 	res3 = strrchr(s, c);
-	if (res2 != res3)
+	printf("Test : \'%c\' in \"%s\" { %p - %p }", c, s, res2, res3);
+	if (res2 != res3) {
 		++res;
-	printf("Test : \"%s\" { %p - %p }", s, res2, res3);
+	}
 	ft_print_status(res);
 	return (res);
 }
 
 int	ft_test_strrchr(void)
 {
-	int		res;
-	const char	*str = "Hello world !";
+	int			res;
+	const char	str[] = "Hello world !";
+	int			c;
 
 	res = 0;
 	ft_print_begin("ft_strrchr");
+	c = 'a';
 	res += ft_test_strrchr2(str, 'H');
-	res += ft_test_strrchr2(str, 'o');
-	res += ft_test_strrchr2(str, 0);
+	while (c <= 'z')
+		res += ft_test_strrchr2(str, c++);
 	res += ft_test_strrchr2(str, '!');
+	res += ft_test_strrchr2(str, 0);
 	return (ft_print_end(res));
 }
